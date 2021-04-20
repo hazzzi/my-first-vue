e8af704902a74d3b2a43b28ab80eeeeffcc0b690
hazzzi <hanee_l@naver.com>
2021-04-20 12:35:42 +0900

    for 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <template>
-	<div>
-		<Header />
-		<article class="wrap">
-			<Box color="blue" />
-			<Box color="purple" />
-			<Box />
-			<Box />
-			<Box />
-		</article>
+	<div id="app">
+		<Webtoon :items="webtoons" />
 	</div>
 </template>
 
 <script>
-import Header from './components/Header';
-import Box from './components/Box';
-
+import Webtoon from './components/Webtoon';
 export default {
-	name: 'App',
 	components: {
-		Header,
-		Box,
+		Webtoon,
+	},
+	data() {
+		return {
+			webtoons: [
+				{
+					name: '햄스터와 그녀',
+					link: 'http://webtoon.daum.net/webtoon/view/hamsterandher',
+					img:
+						'http://t1.daumcdn.net/webtoon/op/478cdf37f585607982ffa9e35b432e8503be8a54',
+				},
+				{
+					name: '프롬 스타',
+					link: 'http://webtoon.daum.net/webtoon/view/fromstar',
+					img:
+						'http://t1.daumcdn.net/webtoon/op/a7fb953d722c1130bfc18440f7e3ce448ece57a1',
+				},
+				{
+					name: '위대한 로맨스',
+					link: 'http://webtoon.daum.net/webtoon/view/greatromance',
+					img:
+						'http://t1.daumcdn.net/webtoon/op/a816281cb4df5c50a20ac386fd6e496643d0f085',
+				},
+				{
+					name: '빛나는 손을',
+					link: 'http://webtoon.daum.net/webtoon/view/Hand',
+					img: 'http://t1.daumcdn.net/cartoon/5913FCAC0234C50001',
+				},
+			],
+		};
 	},
 };
 </script>
 
-<style scoped>
-.wrap {
-	display: flex;
-}
-</style>
+<style scoped></style>
---
+++ src/components/Webtoon.vue
@@ -0,0 +1,65 @@
+<template>
+	<div>
+		<h2>Webtoon</h2>
+		<ul class="wrap">
+			<li
+				class="item"
+				v-for="({ name, link, img }, idx) in items"
+				:key="{ idx }"
+			>
+				<a :href="link" target="_blank">
+					<img :src="img" />
+					<span class="title">제목: {{ name }}</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		items: { type: Array, default: () => [] },
+	},
+};
+</script>
+
+<style scoped>
+h2 {
+	text-align: center;
+}
+
+a {
+	list-style: none;
+	text-decoration: none;
+}
+
+li {
+	list-style: none;
+}
+
+.wrap {
+	max-width: 450px;
+	width: 100%;
+	margin: 0 auto;
+}
+
+.item {
+	border-bottom: 1px solid #ebebeb;
+	margin-bottom: 25px;
+}
+
+.title {
+	display: inline-block;
+	font-size: 18px;
+	font-weight: bold;
+	color: #000;
+	padding: 20px 15px;
+}
+
+img {
+	width: 100%;
+	background: #ebebeb;
+	border-radius: 4px;
+}
+</style>
