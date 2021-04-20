98ef4633677d1371f153af398f5484796d474adf
hazzzi <hanee_l@naver.com>
2021-04-20 12:13:18 +0900

    컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
-// html
 <template>
 	<div>
-		<h1>Hello Vue</h1>
+		<Header />
+		<article class="wrap">
+			<Menu />
+			<Content />
+		</article>
 	</div>
 </template>
 
 <script>
+import Header from './components/Header';
+import Content from './components/Content';
+import Menu from './components/Menu';
+
 export default {
 	name: 'App',
-	components: {},
+	components: {
+		Header,
+		Content,
+		Menu,
+	},
 };
 </script>
 
-<style></style>
+<style scoped>
+.wrap {
+	display: flex;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>Header component</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
